666ecc54d7a04cedf2ba0969ec7f35323afd630f
Evan L Coakley <evancoakley1@gmail.com>
2017-08-04 15:53:06 -0500

    javascript finished

--- main.js
+++ main.js
@@ -1,4 +1,62 @@
+var keys = document.querySelectorAll('.button');
+var screen = document.querySelector('.screen');
+var equal = document.querySelector('.equal');
+var del = document.querySelector('.delete');
 
 
-  
-  
+for (var i = 0; i < keys.length; i++) {
+    keys[i].addEventListener('click', function () {
+        output = this.innerHTML;
+        screen.innerHTML += output;
+
+
+    });
+
+    equal.addEventListener('click', function () {
+        screen.innerHTML = eval(screen.innerHTML)
+
+
+    });
+
+    del.addEventListener('click', function () {
+        screen.innerHTML = ""
+
+
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     let key = key[i];
+//     if (key.classname.includes("button")) {
+//         key.addEventListener('click', function () {
+//             key.innerHTML += display;
+
+
+
+
+//         });
+//     }
+
+// }
+
+
+
